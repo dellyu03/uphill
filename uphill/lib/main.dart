107a4428b6eac6659c219d1f0bc6555f9f9a136d
dellyu03 <seunghwan081@gmail.com>
2026-01-07 11:42:34 +0900

    refactor:change folder location

--- uphill/lib/main.dart
+++ uphill/lib/main.dart
@@ -8,9 +8,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
